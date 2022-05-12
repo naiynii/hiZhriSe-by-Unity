f64a04d7fa7a34a91e1ebf9ae802ead3501c7443
Phatthadon Maliwan <pattadon.mali.43@gmail.com>
2022-05-13 05:27:07 +0700

    Add source code, .dll file

--- Assets/Scripts/SongManager.cs
+++ Assets/Scripts/SongManager.cs
@@ -1,23 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using System.IO;
+using UnityEngine.Networking;
+using System;
 
 public class SongManager : MonoBehaviour
 {
     public static SongManager Instance;
     public AudioSource audioSource;
+    public Lane[] lanes;
     public float songDelayInSeconds;
+    public double marginOfError; // in seconds
+
     public int inputDelayInMilliseconds;
+    
+
     public string fileLocation;
     public float noteTime;
-    public float noteSpawnY1;
+    public float noteSpawnY;
+    public float noteTapY;
+    public float noteDespawnY
+    {
+        get
+        {
+            return noteTapY - (noteSpawnY - noteTapY);
+        }
+    }
+
+    public static MidiFile midiFile;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instance = this;
+        ReadFromFile();
     }
 
-    // Update is called once per frame
+    private void ReadFromFile()
+    {
+        midiFile = MidiFile.Read(Application.streamingAssetsPath + "/" + fileLocation);
+        GetDataFromMidi();
+    }
+    public void GetDataFromMidi()
+    {
+        var notes = midiFile.GetNotes();
+        var array = new Melanchall.DryWetMidi.Interaction.Note[notes.Count];
+        notes.CopyTo(array, 0);
+
+        foreach (var lane in lanes) lane.SetTimeStamps(array);
+
+        Invoke(nameof(StartSong), songDelayInSeconds);
+    }
+    public void StartSong()
+    {
+        audioSource.Play();
+    }
+    public static double GetAudioSourceTime()
+    {
+        return (double)Instance.audioSource.timeSamples / Instance.audioSource.clip.frequency;
+    }
+
     void Update()
     {
         
