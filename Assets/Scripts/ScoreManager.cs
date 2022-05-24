92f93f75b93c7efc7172d4fb3016be8722f4f530
Phatthadon Maliwan <pattadon.mali.43@gmail.com>
2022-05-24 16:52:05 +0700

    Add Loop Background

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,24 +8,31 @@ public class ScoreManager : MonoBehaviour
     public AudioSource hitSFX;
     public AudioSource missSFX;
     public TMPro.TextMeshPro scoreText;
+    public TMPro.TextMeshPro comboText;
+    static int totalScore;
     static int comboScore;
+    
     void Start()
     {
         Instance = this;
+        totalScore = 0;
         comboScore = 0;
     }
     public static void Hit()
     {
         comboScore += 1;
+        totalScore += 200 * comboScore;
         Instance.hitSFX.Play();
     }
     public static void Miss()
     {
+        totalScore += 0;
         comboScore = 0;
         Instance.missSFX.Play();    
     }
     private void Update()
     {
-        scoreText.text = comboScore.ToString();
+        scoreText.text = totalScore.ToString();
+        comboText.text = comboScore.ToString();
     }
 }
