e93b41b25e09c5ed522755d608b124cec2e64cd4
Phatthadon Maliwan <pattadon.mali.43@gmail.com>
2022-06-23 20:36:22 +0700

    jimmy update

--- Assets/Scripts/HighestScore.cs
+++ Assets/Scripts/HighestScore.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class HighestScore : MonoBehaviour
 {
-    
+    public void Song0()
+    {
+
+    }
+    public void Song1()
+    {
+        
+    }
+    public void Song2()
+    {
+        
+    }
+    public void Song3()
+    {
+        
+    }
+
+    public void Song4()
+    {
+        
+    }
 }
